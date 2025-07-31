7204cfa92dc148861773685681c6da6ceeb80614
pari55051 <xpari5505@gmail.com>
2025-07-31 10:51:14 +0530

    added pseudo for main page logic in main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// PSEUDO for running random game
+// game1 = link to game 1
+// :
+// games = [game1, game2, game3, game4]
+// getRandom(index)
+// run(games[randomlySelectedGame])
+
+// PSEUDO for red btn
+// get redBtn elem
+// numClicks = random(7-10/7-15)
+// if clicks > numClicks:
+//  getRandomGame
+// if clicks <= numClicks:
+//  displayMessage(random from message[])
